ee4bea6506cf87a39dbd173794f0daa23ab7cea6
nith2001 <nith2001@uw.edu>
2023-01-26 13:19:19 -0800

    Logo and title edits and styling

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -53,10 +53,10 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Husky Coding Project',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/clubwebsite.jpeg',
         },
         items: [
           {
