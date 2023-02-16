6f4b3b57ead85e450f771e518afc22844e833797
nith2001 <nith2001@uw.edu>
2023-02-16 09:01:19 -0800

    Styling and deployment stuff

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -8,16 +8,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Husky Coding Project',
   tagline: 'Let\'s Git Good',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://nith2001.github.io',
+  baseUrl: '/hcp-documentation/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'HCP Leadership Docs', // Usually your repo name.
+  organizationName: 'nith2001', // Usually your GitHub org/user name.
+  projectName: 'hcp-documentation', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
