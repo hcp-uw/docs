45a1fb38ec7d321cc396c1ed1d3dd2cc887e8bcc
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-05-17 17:10:25 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,15 +9,15 @@ const config = {
   title: 'Husky Coding Project',
   tagline: 'Let\'s Git Good',
   url: 'https://hcp-uw.github.io/',
-  baseUrl: '/hcp-documentation/',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'nith2001', // Usually your GitHub org/user name.
-  projectName: 'hcp-documentation', // Usually your repo name.
+  organizationName: 'hcp-uw', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
